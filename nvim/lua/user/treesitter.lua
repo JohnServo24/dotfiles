05cccb1cacd1f4fea37b4900819e865de0fb3ea4
John Servo <johnservo.se@gmail.com>
2025-03-10 23:04:59 +0800

    Enabled autotag in nvim

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,11 +1,11 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+    return
 end
 
 -- configure treesitter
 treesitter.setup({
-	ensure_installed = {
+    ensure_installed = {
         "javascript",
         "typescript",
         "tsx",
@@ -18,24 +18,24 @@ treesitter.setup({
         "go",
         "java"
     },
-	auto_install = true,
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-		disable = { "" },
-		additional_vim_regex_highlighting = true,
-	},
-	-- enable indentation
-	indent = {
+    auto_install = true,
+    -- enable syntax highlighting
+    highlight = {
+        enable = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = true,
+    },
+    -- enable indentation
+    indent = {
         enable = true
     },
-	-- enable autopairs
-	autopairs = {
-		enable = true,
-	},
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	-- autotag = {
- --        enable = true
- --    },
-    sync_install = false,
+    -- enable autopairs
+    autopairs = {
+        enable = true,
+    },
+    -- enable autotagging (w/ nvim-ts-autotag plugin)
+    autotag = {
+        enable = true
+    },
+    sync_install = false
 })
